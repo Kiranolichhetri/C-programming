08e02aac3d2dbc393e52287f8c765d02c5d72066
Kiranolichhetri <137617846+Kiranolichhetri@users.noreply.github.com>
2024-02-04 21:44:56 +0545

    Update FibonacciSeries.c

--- FibonacciSeries.c
+++ FibonacciSeries.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a=0,b=1,c;
+    printf("Enter the number of term:");  /* let n=5 */
+    scanf("%d",&n);
+    for(i=1;i<=n;i++) /* (1<=5 2<=5 3<=5 4<=5 5<=5) true (6<=5) false */
+    {
+        printf("%d",a); /* 0 */
+        c = a+b; /* c=1 */
+        a=b; /* a=1 */
+        b=c; /*b=1 */
+    }
+    return 0;
+      
+}
 
